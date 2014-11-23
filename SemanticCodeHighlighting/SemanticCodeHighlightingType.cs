791670c33832f08cffe6472703521c2c940a9dd8
Jedy <jordangeorgiev92@gmail.com>
2014-11-24 00:55:05 +0200

    Initial commit

---
+++ SemanticCodeHighlighting/SemanticCodeHighlightingType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SemanticCodeHighlighting {
+	internal static class SemanticCodeHighlightingClassificationDefinition {
+		/// <summary>
+		/// Defines the "SemanticCodeHighlighting" classification type.
+		/// </summary>
+		[Export(typeof(ClassificationTypeDefinition))]
+		[Name("SemanticCodeHighlighting")]
+		internal static ClassificationTypeDefinition SemanticCodeHighlightingType = null;
+	}
+}
---
+++ SemanticCodeHighlighting/TextViewCreationListener.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SemanticCodeHighlighting {
+	[Export(typeof(IWpfTextViewCreationListener))]
+	[ContentType("code")]
+	[TextViewRole(PredefinedTextViewRoles.Editable)] //Editable for now
+	public class TextViewCreationListener : IWpfTextViewCreationListener {
+
+		public void TextViewCreated(IWpfTextView textView) {
+			
+
+
+		}
+	}
+}
