4e9cce3aa64b35b6afff9f9f7c71267f4441fd48
Jedy <jordangeorgiev92@gmail.com>
2014-11-30 01:24:14 +0200

    More renaming to keep it simple.

---
+++ SemanticCodeHighlighting/ClassificationFormats.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SemanticCodeHighlighting {
+	internal static class ClassificationFormats {
+		#region Format definition
+		/// <summary>
+		/// Defines an editor format for the SemanticCodeHighlighting type that has a purple background
+		/// and is underlined.
+		/// </summary>
+		[Export(typeof(EditorFormatDefinition))]
+		[ClassificationType(ClassificationTypeNames = "SemanticCodeHighlighting")]
+		[Name("SemanticCodeHighlighting")]
+		[UserVisible(true)] //this should be visible to the end user
+		[Order(Before = Priority.Default)] //set the priority to be after the default classifiers
+		internal sealed class FormatDefinition : ClassificationFormatDefinition {
+			/// <summary>
+			/// Defines the visual format for the "SemanticCodeHighlighting" classification type
+			/// </summary>
+			public FormatDefinition() {
+				DisplayName = "Semantic Code Highlighting"; //human readable version of the name
+				BackgroundColor = Colors.BlueViolet;
+				TextDecorations = System.Windows.TextDecorations.Underline;
+			}
+		}
+		#endregion //Format definition
+	}
+}
