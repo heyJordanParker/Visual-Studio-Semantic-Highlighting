f7b99fff2a898edcba2972d519b5a5c4633d5e6c
Jedy <jordangeorgiev92@gmail.com>
2014-12-01 01:35:51 +0200

    Testing functionality. Currently making every variable bigger and bold.

---
+++ SemanticCodeHighlighting/FormatMapWatcher.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace SemanticCodeHighlighting {
+
+	class FormatMapWatcher {
+		private readonly IClassificationFormatMap _formatMap;
+		private IClassificationType _baseClassificationType;
+		private IClassificationTypeRegistryService _typeRegistry;
+		private Colorizer _colorizer;
+		private bool _updating;
+
+		public FormatMapWatcher(IWpfTextView textView, IClassificationFormatMap formatMap, IClassificationTypeRegistryService typeRegistry) {
+			_formatMap = formatMap;
+			_typeRegistry = typeRegistry;
+			_baseClassificationType = typeRegistry.GetClassificationType("SemanticCodeHighlighting");
+			_colorizer = textView.Properties.GetOrCreateSingletonProperty(() => new Colorizer());
+			
+			_formatMap.ClassificationFormatMappingChanged += FormatMapChanged;
+			textView.GotAggregateFocus += OnFirstFocus;
+		}
+
+		private void OnFirstFocus(object textView, EventArgs e) {
+			((ITextView) textView).GotAggregateFocus -= OnFirstFocus;
+
+			Colorize();
+		}
+
+		private void FormatMapChanged(object sender, EventArgs eventArgs) {
+			if(!_updating) {
+				Colorize();
+			}
+
+		}
+
+		//TODO move to Colorizer
+		private void Colorize() {
+			try {
+				_updating = true;
+
+				foreach(var classification in _formatMap.CurrentPriorityOrder.Where(c => c != null)) {
+					string name = classification.Classification.ToLowerInvariant();
+					if(name.Contains("variable")) {
+						Bold(classification);
+					}
+				}
+
+
+			} finally {
+				_updating = false;
+			}
+		}
+
+		private void Bold(IClassificationType classification) {
+			var textFormat = _formatMap.GetTextProperties(_typeRegistry.GetClassificationType("text"));
+			var properties = _formatMap.GetTextProperties(classification);
+			var typeface = properties.Typeface;
+
+			var boldedTypeface = new Typeface(typeface.FontFamily, typeface.Style, FontWeights.Bold, typeface.Stretch);
+			var biggerSize = textFormat.FontRenderingEmSize + 2;
+
+			properties = properties.SetTypeface(boldedTypeface);
+			properties = properties.SetFontRenderingEmSize(biggerSize);
+
+			_formatMap.SetTextProperties(classification, properties);
+		}
+	}
+}
--- SemanticCodeHighlighting/ViewCreationListener.cs
+++ SemanticCodeHighlighting/ViewCreationListener.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.Composition;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
 
 namespace SemanticCodeHighlighting {
@@ -8,19 +9,22 @@ namespace SemanticCodeHighlighting {
 	/// This class causes a classifier to be added to the set of classifiers. Since 
 	/// the content type is set to "text", this classifier applies to all text files
 	/// </summary> [
-	[Export(typeof(IClassifierProvider))]
+	[Export(typeof(IWpfTextViewCreationListener))]
 	[ContentType("code")]
-	internal class Provider : IClassifierProvider {
+	[TextViewRole(PredefinedTextViewRoles.Document)]
+	internal class ViewCreationListener : IWpfTextViewCreationListener {
 		/// <summary>
 		/// Import the classification registry to be used for getting a reference
 		/// to the custom classification type later.
 		/// </summary>
 		[Import]
-		internal IClassificationTypeRegistryService classificationRegistry; // Set via MEF
+		internal IClassificationTypeRegistryService typeRegistry; // Set via MEF
 
-		public IClassifier GetClassifier(ITextBuffer textBuffer) {
-			return textBuffer.Properties.GetOrCreateSingletonProperty(
-				() => new Classifier(textBuffer, classificationRegistry));
+		[Import] internal IClassificationFormatMapService formatMapService;
+
+		public void TextViewCreated(IWpfTextView textView) {
+			textView.Properties.GetOrCreateSingletonProperty(
+				() => new FormatMapWatcher(textView, formatMapService.GetClassificationFormatMap(textView), typeRegistry));
 		}
 	}
 }
